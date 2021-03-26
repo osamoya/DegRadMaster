88989598ac6a56f8b7332e26343d0d52fed124a5
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-03-26 18:19:26 +0900

    [Add]GMスクリプトを作成
    色々なものがここに集結しそうです

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public RD_ManagerScript RD_;
+    public R_QuestionScript R_Q_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void retry()
+    {
+        R_Q_.setQ1();
+        R_Q_.stage1();
+        Debug.Log("分母："+RD_.denominator+"に設定");
+    }
+}
