bca39ebc17a1e9d42e48b8e51e4a25fc9edbaaef
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-03-26 18:36:19 +0900

    [update]選択肢クリック時の反応を追加
    直接代入してしまうことに。そのため、選択肢ごとにアタッチが必須

--- Assets/Scripts/R_ChoiceDate.cs
+++ Assets/Scripts/R_ChoiceDate.cs
@@ -10,9 +10,15 @@ public class R_ChoiceDate : MonoBehaviour
 {
     [SerializeField] int denominator;
     [SerializeField] int numerator;//これでゲッターになる？
+    public RD_ManagerScript RDM_;
 
+    public void Onclick()
+    {
+        RDM_.inputDen = denominator;
+        RDM_.inputNum = numerator;
+    }
 
-    public R_ChoiceDate OnChoice()
+    public R_ChoiceDate getChoice()
     {
         return this;
     }
