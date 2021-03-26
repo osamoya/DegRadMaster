435fb17735a0554121221930803ed8f9f8934816
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-03-26 14:44:14 +0900

    [Add]分数を決めるスクリプトを作成
    分母となる数字の配列と、分母と象限から分子を決めるメソッド
    
    ただし、分母が2の時には気を付ける必要がありそう
    また、分母が12には対応していない

---
+++ Assets/Scripts/R_SetsScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ラジアンのための分母がいくつか入っている
+/// 分子はまだ考えない
+/// </summary>
+public class R_SetsScript : MonoBehaviour
+{
+    //まずは分母
+    public int[] first = { 2,3,4,6};//基本この人たち
+    public int[] second = {12};//応用の時にはこれも
+
+
+    //分母と象限から、適切な分子を返却する。
+    //ただし、第1象限とは0<=θ<=90とする
+    //                    分母　　象限
+    int CalcNumerator(int den,int Quadrant)
+    {
+        if (Quadrant < 1 || Quadrant > 4) { Debug.Log("象限の設定ミス"); return -1; }
+        
+        //TODO:分母12には対応していません！
+        switch (Quadrant)
+        {
+            case 1:return 1;
+            case 2:
+                if (den == 2) return 1;
+                return den - 1;
+            case 3:
+                return den + 1;
+            case 4:
+                if (den == 2) return 2;
+                return den * 2 - 1;
+        }
+        return 0;
+    }
+
+}
