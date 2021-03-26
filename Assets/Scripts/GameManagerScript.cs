d36efd4217444db4763329b1704e3c7b9bebcc64
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-03-26 18:48:59 +0900

    [update]ボタンでリフレッシュされるように
    アタッチし、データから角度を引っ張ってきた

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
     public RD_ManagerScript RD_;
     public R_QuestionScript R_Q_;
-
+    public Text Source;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +25,6 @@ public class GameManagerScript : MonoBehaviour
         R_Q_.setQ1();
         R_Q_.stage1();
         Debug.Log("分母："+RD_.denominator+"に設定");
+        Source.text = "" + RD_.deg;
     }
 }
