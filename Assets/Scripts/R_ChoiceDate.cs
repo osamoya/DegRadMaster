b3d407f4399dbfafd7c219bdf5d612bc8e70719e
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-03-26 17:52:10 +0900

    [Add]選択肢データを保持するスクリプトを作成
    単純に情報を持たせた。また、クリック時の動作としてマネージャーの関数を呼ぶようにしたい。
    
    オブジェクトまたはこのスクリプト自体を代入させよう

---
+++ Assets/Scripts/R_ChoiceDate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 選択しのデータを保持するだけ
+/// オブジェクトを参照し、
+/// これのデータを読みとる
+/// </summary>
+public class R_ChoiceDate : MonoBehaviour
+{
+    [SerializeField] int denominator { get;}
+    [SerializeField] int numerator { get; }//これでゲッターになる？
+
+
+    public R_ChoiceDate OnChoice()
+    {
+        return this;
+    }
+}
