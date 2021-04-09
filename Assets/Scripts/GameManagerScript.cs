5c3a3477bdeab3e54ffbda5f2940a174c126ad88
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-04-09 18:59:18 +0900

    [Add]問題数の宣言と、開始時に初期化
    ただし、この方法では毎回角度から弧度法であるため、拡張するなら考えたいところ

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -10,10 +10,11 @@ public class GameManagerScript : MonoBehaviour
     public RD_ManagerScript RD_;
     public R_QuestionScript R_Q_;
     public Text Source;
+    private int Qnumber;
     // Start is called before the first frame update
     void Start()
     {
-        
+        retry();
     }
 
     // Update is called once per frame
@@ -28,5 +29,6 @@ public class GameManagerScript : MonoBehaviour
         R_Q_.stage1();
         Debug.Log("分母："+RD_.denominator+"に設定");
         Source.text = "" + RD_.deg;
+        Qnumber = 0;
     }
 }
